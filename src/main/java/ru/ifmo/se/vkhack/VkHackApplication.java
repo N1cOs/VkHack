8fd003c7ae837c99b950fb258f5f0b834462ce6e
N1cOs <N1cOs@users.noreply.github.com>
2018-11-09 14:41:49 +0300

    first commit

---
+++ src/main/java/ru/ifmo/se/vkhack/VkHackApplication.java
@@ -0,0 +1,12 @@
+package ru.ifmo.se.vkhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkHackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkHackApplication.class, args);
+    }
+}
