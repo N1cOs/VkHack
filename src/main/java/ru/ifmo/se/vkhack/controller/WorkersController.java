e5a86c1ed91921a44238e178349222e821d83cae
N1cOs <N1cOs@users.noreply.github.com>
2018-11-10 08:59:52 +0300

    added web config and improved search answer

--- src/main/java/ru/ifmo/se/vkhack/controller/WorkersController.java
+++ src/main/java/ru/ifmo/se/vkhack/controller/WorkersController.java
@@ -3,19 +3,15 @@ package ru.ifmo.se.vkhack.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.ifmo.se.vkhack.domain.Worker;
 import ru.ifmo.se.vkhack.repository.WorkerRepository;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-
 @Controller
-@RequestMapping("/employee")
 public class WorkersController {
     private final WorkerRepository workerRepository;
 
@@ -24,12 +20,7 @@ public class WorkersController {
         this.workerRepository = workerRepository;
     }
 
-    @GetMapping
-    public String getEmployees(Model model){
-        return "search";
-    }
-
-    @PostMapping("/search")
+    @RequestMapping(value = "/employee/search", method = RequestMethod.POST)
     public String getSuitableEmployees(@RequestParam("search") String searchingInfo, Model model){
         Collection<Worker> workers = workerRepository.findAllBySurname("Иванов");
         model.addAttribute("workers", workers);
