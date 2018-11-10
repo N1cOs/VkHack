83cb21fe9c24ba13784cd64dfcfae2a2189aaa69
N1cOs <N1cOs@users.noreply.github.com>
2018-11-10 15:28:33 +0300

    fixed references

--- src/main/java/ru/ifmo/se/vkhack/config/WebConfig.java
+++ src/main/java/ru/ifmo/se/vkhack/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/employee").setViewName("search");
-        registry.addViewController("/").setViewName("home");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/map").setViewName("map");
     }
 }
