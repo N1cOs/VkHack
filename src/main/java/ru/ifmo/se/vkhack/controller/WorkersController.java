62af3d3918cef200369a437628ef944f14a1e784
N1cOs <N1cOs@users.noreply.github.com>
2018-11-10 09:15:46 +0300

    added logic to search worker

--- src/main/java/ru/ifmo/se/vkhack/controller/WorkersController.java
+++ src/main/java/ru/ifmo/se/vkhack/controller/WorkersController.java
@@ -3,14 +3,15 @@ package ru.ifmo.se.vkhack.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.ifmo.se.vkhack.domain.Worker;
 import ru.ifmo.se.vkhack.repository.WorkerRepository;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class WorkersController {
     private final WorkerRepository workerRepository;
@@ -22,8 +23,15 @@ public class WorkersController {
 
     @RequestMapping(value = "/employee/search", method = RequestMethod.POST)
     public String getSuitableEmployees(@RequestParam("search") String searchingInfo, Model model){
-        Collection<Worker> workers = workerRepository.findAllBySurname("Иванов");
-        model.addAttribute("workers", workers);
+        Iterable<Worker> workers = workerRepository.findAll();
+        List<Worker> matchWorkers = new ArrayList<>();
+        workers.forEach(worker -> {
+            if(worker.getName().equalsIgnoreCase(searchingInfo) || worker.getSurname().equalsIgnoreCase(searchingInfo) ||
+                    worker.getPatronymic().equalsIgnoreCase(searchingInfo)){
+                matchWorkers.add(worker);
+            }
+        });
+        model.addAttribute("workers", matchWorkers);
         return "searchAnswer";
     }
 }
